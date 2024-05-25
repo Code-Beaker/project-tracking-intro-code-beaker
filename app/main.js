6a3ea181d565915ba1a1e104eca6e50d319083a5
Tharun Raj <tharun.2.linux@gmail.com>
2024-05-25 10:35:46 +0530

    finish project

--- app/main.js
+++ app/main.js
@@ -1,1 +1,7 @@
 import "./styles/style.css";
+const navToggler = document.getElementById("toggleButton");
+const navLinks = document.querySelector(".navbar__links");
+
+navToggler.addEventListener("click", () => {
+  navLinks.classList.toggle("navbar__links-show");
+});
